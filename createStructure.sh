5400260b4cb6a7c8ceb2c899f5da233cb7f88885
Tuna <tuannhanle@gmail.com>
2023-04-25 11:13:53 +0700

    create bash file for create the necessary folders

---
+++ createStructure.sh
@@ -0,0 +1,18 @@
+mkdir -p Assets/App/Scripts/Domains
+mkdir -p Assets/App/Scripts/GameControllers
+mkdir -p Assets/App/Scripts/Infrastructures
+mkdir -p Assets/App/Scripts/UI
+mkdir -p Assets/App/Scripts/Models
+mkdir -p Assets/App/Scripts/Mics
+mkdir -p Assets/App/Resources
+mkdir -p Assets/App/UI
+mkdir -p Assets/App/Prefabs
+
+mkdir -p Assets/Scenes
+mkdir -p Assets/Tests
+mkdir -p Assets/Config
+mkdir -p Assets/Editor
+mkdir -p Assets/Plugins
+mkdir -p Assets/StreamingAssets
+
+
